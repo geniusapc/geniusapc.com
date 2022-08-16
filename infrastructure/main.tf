532c5438c9ec28947b44ca45496cb4ee1fe86c77
Prince Arthur <geniusapc@gmail.com>
2022-08-16 08:42:14 +0100

    feat(infra): add git backend

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,15 @@
+locals {
+  env = terraform.workspace
+}
+
+terraform {
+  backend "s3" {
+    bucket = "geniusapc-terraform-backend-bucket"
+    key    = "geniusapc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 module "s3_module" {
   app_name     = var.app_name
   source       = "./modules/s3_module"
